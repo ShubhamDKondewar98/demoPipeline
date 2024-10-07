2059a772c6e9eae154edab2eb716db71fb28ffbd
shubham kondewar <shubham.kondewar98@gmail.com>
2024-10-08 02:40:07 +0530

    some chnages

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup , find_packages
+
+setup(name='census-income',version='0.0.1',author='shubham',auther_email='shubhamkondewar98@gmail.com',
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"])
+
+
